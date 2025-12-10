709910e198b8575cf4499e79f82cd93430402928
viylouu <131220600+viylouu@users.noreply.github.com>
2025-12-09 18:17:48 -0700

    a

--- examples/window/main.zig
+++ examples/window/main.zig
@@ -2,25 +2,25 @@ const zg = @import("z_goop");
 
 const std = @import("std");
 
-const game = struct{
-    fn init() !void {
+const Game = struct{
+    pub fn init() !void {
         std.debug.print("init!\n", .{});
     }
 
-    fn update(dt: f32) !void { 
-        std.debug.print("update! {d} FPS\n", .{ 1.0/dt });
+    pub fn update(dt: f32) !void { 
+        std.debug.print("update! {d:.2} FPS\n", .{ 1.0/dt });
     }
 
-    fn exit() !void {
+    pub fn exit() !void {
         std.debug.print("exit!\n", .{});
     }
 };
 
 pub fn main() !void {
     try zg.run(.{
-        .init   = game.init,
-        .update = game.update,
-        .exit   = game.exit,
+        .init   = Game.init,
+        .update = Game.update,
+        .exit   = Game.exit,
 
         .title  = "window",
         .width  = 800,
