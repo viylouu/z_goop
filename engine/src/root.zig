1ad658052e0e5568662a3155988b7dafd0922f53
viylouu <131220600+viylouu@users.noreply.github.com>
2025-12-09 18:23:00 -0700

    anyerror

--- engine/src/root.zig
+++ engine/src/root.zig
@@ -1,21 +1,17 @@
 const std = @import("std");
 
-const EngineError = error{
-    InitFailure,
-    UpdateFailure,
-    ExitFailure
-};
-
 pub fn run(api: struct{
-    init:   fn() EngineError !void,
-    update: fn(dt: f32) EngineError !void,
-    exit:   fn() EngineError !void,
+    init:   fn()        anyerror !void,
+    update: fn(dt: f32) anyerror !void,
+    exit:   fn()        anyerror !void,
 
     title:  []const u8,
     width:  u32,
     height: u32,
 }) !void {
     try api.init();
+
     try api.update(1.0/60.0);
+
     try api.exit();
 }
