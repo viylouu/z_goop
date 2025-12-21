dbff1122945b0d49e48bf30446198e1099e6e8e7
viylouu <131220600+viylouu@users.noreply.github.com>
2025-12-21 15:26:46 -0700

    basic init/deinit so far

--- engine/graah/root.zig
+++ engine/graah/root.zig
@@ -1,4 +1,23 @@
 const std = @import("std");
 const zg = @import("z_goop");
+const zrend = zg.rend;
 
+var state: struct{
+    arena: std.heap.ArenaAllocator,
+    alloc: std.mem.Allocator,
 
+    r: *zrend.Impl,
+} = .{};
+
+pub fn init(desc: struct{ 
+    rend_impl: *zrend.Impl 
+}) !void {
+    state.arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    state.alloc = state.arena.allocator();
+
+    state.r = desc.rend_impl;
+}
+
+pub fn deinit() void {
+    state.arena.deinit();
+}
