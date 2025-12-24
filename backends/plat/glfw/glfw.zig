155c925a54f1fabc3504de9bf00197bf680a0935
viylouu <131220600+viylouu@users.noreply.github.com>
2025-12-24 15:02:11 -0700

    boo!

--- backends/plat/glfw/glfw.zig
+++ backends/plat/glfw/glfw.zig
@@ -16,6 +16,10 @@ pub var impl = zplat.Impl{
     .width        = undefined,
     .height       = undefined,
 
+    .keys         = [_]zplat.KeyState{.Inactive} ** std.meta.fields(zplat.Key).len,
+    .mousebuts    = [_]zplat.KeyState{.Inactive} ** std.meta.fields(zplat.Mouse).len,
+    .mouse        = .{.x = 0, .y = 0},
+
     .make_fn      = Impl.make,
     .delete_fn    = Impl.delete,
 
@@ -104,9 +108,70 @@ const Impl = struct{
         return c.glfwWindowShouldClose(ts.window) != 0;
     }
 
+    fn upk(self: *zplat.Impl, z: zplat.Key, g: c_int) void {
+        const ts: *Impl = @ptrCast(@alignCast(self.act));
+        const i = @intFromEnum(z);
+
+        const p = self.keys[i];
+        self.keys[i] = if(c.glfwGetKey(ts.window, g) == 1) .Pressed else .Released;
+        if (self.keys[i] == .Pressed and (p == .Pressed or p == .Held)) self.keys[i] = .Held;
+        if (self.keys[i] == .Released and (p == .Released or p == .Inactive)) self.keys[i] = .Inactive;
+    }
+
+    fn upm(self: *zplat.Impl, z: zplat.Mouse, g: c_int) void {
+        const ts: *Impl = @ptrCast(@alignCast(self.act));
+        const i = @intFromEnum(z);
+
+        const p = self.mousebuts[i];
+        self.mousebuts[i] = if(c.glfwGetMouseButton(ts.window, g) == 1) .Pressed else .Released;
+        if (self.mousebuts[i] == .Pressed and (p == .Pressed or p == .Held)) self.mousebuts[i] = .Held;
+        if (self.mousebuts[i] == .Released and (p == .Released or p == .Inactive)) self.mousebuts[i] = .Inactive;
+    }
+
     fn poll(self: *zplat.Impl) !void {
-        _ = self;
+        const ts: *Impl = @ptrCast(@alignCast(self.act));
         c.glfwPollEvents();
+
+        upk(self, .Escape, c.GLFW_KEY_ESCAPE);
+        upk(self, .Caps, c.GLFW_KEY_CAPS_LOCK);
+        upk(self, .Space, c.GLFW_KEY_SPACE);
+        upk(self, .Tab, c.GLFW_KEY_TAB);
+        upk(self, .Backspace, c.GLFW_KEY_BACKSPACE);
+        upk(self, .Delete, c.GLFW_KEY_DELETE);
+        upk(self, .Home, c.GLFW_KEY_HOME);
+        upk(self, .End, c.GLFW_KEY_END);
+        upk(self, .PageUp, c.GLFW_KEY_PAGE_UP);
+        upk(self, .PageDown, c.GLFW_KEY_PAGE_DOWN);
+        upk(self, .Insert, c.GLFW_KEY_INSERT);
+
+        upk(self, .LShift, c.GLFW_KEY_LEFT_SHIFT);
+        upk(self, .RShift, c.GLFW_KEY_RIGHT_SHIFT);
+        upk(self, .LCtrl, c.GLFW_KEY_LEFT_CONTROL);
+        upk(self, .RCtrl, c.GLFW_KEY_RIGHT_CONTROL);
+        upk(self, .LAlt, c.GLFW_KEY_LEFT_ALT);
+        upk(self, .RAlt, c.GLFW_KEY_RIGHT_ALT);
+        upk(self, .LSuper, c.GLFW_KEY_LEFT_SUPER);
+        upk(self, .RSuper, c.GLFW_KEY_RIGHT_SUPER);
+
+        upk(self, .Up, c.GLFW_KEY_UP);
+        upk(self, .Left, c.GLFW_KEY_LEFT);
+        upk(self, .Right, c.GLFW_KEY_RIGHT);
+        upk(self, .Down, c.GLFW_KEY_DOWN);
+
+        for (@intFromEnum(zplat.Key.A)..@intFromEnum(zplat.Key.Z)) |k| upk(self, @enumFromInt(k), @as(c_int, @intCast(k))-c.GLFW_KEY_A);
+        for (@intFromEnum(zplat.Key.K1)..@intFromEnum(zplat.Key.K9)) |k| upk(self, @enumFromInt(k), @as(c_int, @intCast(k))-c.GLFW_KEY_1);
+            upk(self, .K0, c.GLFW_KEY_0);
+        for (@intFromEnum(zplat.Key.F1)..@intFromEnum(zplat.Key.F12)) |k| upk(self, @enumFromInt(k), @as(c_int, @intCast(k))-c.GLFW_KEY_F1);
+
+        upm(self, .Left, c.GLFW_MOUSE_BUTTON_LEFT);
+        upm(self, .Right, c.GLFW_MOUSE_BUTTON_RIGHT);
+        upm(self, .Middle, c.GLFW_MOUSE_BUTTON_MIDDLE);
+
+        var mx: f64 = @floatCast(self.mouse.x);
+        var my: f64 = @floatCast(self.mouse.y);
+        c.glfwGetCursorPos(ts.window, &mx, &my);
+        self.mouse.x = @floatCast(mx);
+        self.mouse.y = @floatCast(my);
     }
     fn swap(self: *zplat.Impl) !void {
         const ts: *Impl = @ptrCast(@alignCast(self.act));
--- engine/src/plat.zig
+++ engine/src/plat.zig
@@ -1,5 +1,48 @@
 const std = @import("std");
 const rend = @import("rend.zig");
+const math = @import("math.zig");
+
+pub const KeyState = enum{
+    Released,
+    Pressed,
+    Held,
+    Inactive,
+};
+pub const Key = enum{
+    Escape,
+    Caps,
+    Space, Tab,
+    Backspace, Delete,
+    Home, End,
+    PageUp, PageDown,
+    Insert,
+
+    LShift, RShift,
+    LCtrl, RCtrl,
+    LAlt, RAlt,
+    LSuper, RSuper,
+
+    Up, Left, Right, Down,
+
+    A, B, C, D, E,
+    F, G, H, I, J,
+    K, L, M, N, O,
+    P, Q, R, S, T,
+    U, V, W, X, Y, Z,
+    
+    K1, K2, K3, K4, K5,
+    K6, K7, K8, K9, K0,
+
+    F1, F2, F3, F4,
+    F5, F6, F7, F8,
+    F9, F10, F11, F12,
+
+    // add symbols and stuff
+};
+
+pub const Mouse = enum{
+    Left, Right, Middle,
+};
 
 pub const Impl = struct{
     pub fn make(self: *Impl, r_impl: *rend.Impl, width: u32, height: u32, title: [:0]const u8) anyerror !void { 
@@ -30,6 +73,10 @@ pub const Impl = struct{
 
     width: u32,
     height: u32,
+
+    keys: [std.meta.fields(Key).len]KeyState,
+    mousebuts: [std.meta.fields(Mouse).len]KeyState,
+    mouse: math.Vec2,
 
     make_fn:   *const fn(self: *Impl, r_impl: *rend.Impl, width: u32, height: u32, title: [:0]const u8) anyerror !void,
     delete_fn: *const fn(self: *Impl) void,
